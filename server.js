7994de320aa2bbe0c087240de970388730a6db32
pfieffer <cbpmgr@gmail.com>
2019-05-22 01:58:31 +0545

    Create post message service

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
-var express = require('express')
+var express = require('express') //express package
+var bodyParser = require('body-parser') //body parser package
 var app = express()
 
 app.use(express.static(__dirname))
+app.use(bodyParser.json()) //we expect json to be coming in
+app.use(bodyParser.urlencoded({extended: false}))
 
 var messages =[
     {name: 'Tim', message: 'Hi'},
@@ -12,6 +15,11 @@ app.get('/messages', (req, res)=>{
     res.send(messages)
 })
 
+app.post('/messages', (req, res)=>{
+    
+    messages.push(req.body)
+    res.sendStatus(200)
+})
 
 var server = app.listen(3000, () => {
     console.log('Server is listening on port', server.address().port)
