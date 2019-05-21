8c8086eac35e13a4aa462031975b6a89292f7ee2
pfieffer <cbpmgr@gmail.com>
2019-05-22 02:31:46 +0545

    Setup MongoDB

--- server.js
+++ server.js
@@ -3,10 +3,13 @@ var bodyParser = require('body-parser') //body parser package
 var app = express()
 var http = require('http').Server(app)
 var io = require('socket.io')(http)
+var mongoose = require('mongoose')
 
 app.use(express.static(__dirname))
 app.use(bodyParser.json()) //we expect json to be coming in
 app.use(bodyParser.urlencoded({extended: false}))
+
+var dbUrl = 'mongodb+srv://user:user@cluster0-kwipy.mongodb.net/test?retryWrites=true'
 
 var messages =[
     {name: 'Tim', message: 'Hi'},
@@ -25,6 +28,10 @@ app.post('/messages', (req, res)=>{
 
 io.on('connection', (socket) => {
     console.log('a user connected')
+})
+
+mongoose.connect(dbUrl, (err) => {
+    console.log('mongo db connection', err)
 })
 
 var server = http.listen(3000, () => {
